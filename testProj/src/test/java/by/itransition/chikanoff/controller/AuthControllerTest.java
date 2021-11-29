f63f0dda8e3e5bee0dc2160e5cee2fcfd55c2b9c
chikanoff <hhappyzor@gmail.com>
2021-11-29 10:47:46 +0300

    trying to fix

--- testProj/src/test/java/by/itransition/chikanoff/controller/AuthControllerTest.java
+++ testProj/src/test/java/by/itransition/chikanoff/controller/AuthControllerTest.java
@@ -8,24 +8,23 @@ import by.itransition.chikanoff.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 public class AuthControllerTest {
     @Autowired
     private MockMvc mvc;
--- testProj/src/test/java/by/itransition/chikanoff/service/UserServiceIntegrationTest.java
+++ testProj/src/test/java/by/itransition/chikanoff/service/UserServiceIntegrationTest.java
@@ -5,23 +5,22 @@ import by.itransition.chikanoff.payloads.request.SignupRequest;
 import by.itransition.chikanoff.repository.UserRepository;
 import by.itransition.chikanoff.services.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 public class UserServiceIntegrationTest {
     @MockBean
     private UserRepository userRepository;
