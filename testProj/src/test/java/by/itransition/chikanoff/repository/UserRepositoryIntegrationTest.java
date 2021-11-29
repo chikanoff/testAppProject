59f481ff3f6ba65e3890b7edca631977e82c4aa8
chikanoff <hhappyzor@gmail.com>
2021-11-29 17:29:10 +0300

    fix 2.0.0

--- testProj/src/test/java/by/itransition/chikanoff/repository/UserRepositoryIntegrationTest.java
+++ testProj/src/test/java/by/itransition/chikanoff/repository/UserRepositoryIntegrationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DataJpaTest
+@SpringBootTest
 public class UserRepositoryIntegrationTest extends IntegrationTestBase {
 
     @Autowired
