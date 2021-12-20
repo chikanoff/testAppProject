0b25e6966600971a7cdbd8af85c3fc00f5bde98b
chikanoff <hhappyzor@gmail.com>
2021-12-20 10:52:56 +0300

    fix overriding bean

--- testProj/src/test/java/by/itransition/chikanoff/utils/RabbitConfig.java
+++ testProj/src/test/java/by/itransition/chikanoff/utils/RabbitConfig.java
@@ -4,27 +4,17 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class RabbitConfig {
-    @Value("${rabbitmq.queue}")
-    private String queueName;
-
     @Value("${rabbitmq.exchange}")
     private String exchange;
 
     @Value("${rabbitmq.routingkey}")
     private String routingKey;
-
-    @Bean
-    public Queue queue() {
-        return new Queue(queueName, false);
-    }
 
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange) {
@@ -34,10 +24,5 @@ public class RabbitConfig {
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(exchange);
-    }
-
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 }
